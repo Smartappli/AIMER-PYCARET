e28dae487067d053ab55233a06a5805b7623e010
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-15 10:09:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,28 @@ class ModelType(str, Enum):
     regression = "regression"
     clustering = "clustering"
 
+
+class ModelClassification(str, Enum):
+    lr = "Logistic Regression"
+    knn = "K Neighbors Classifier"
+    nb = "Naive Bayes"
+    dt = "Decision Tree Classifier"
+    svm = "SVM - Linear Kernel"
+    rbfsvm = "SVM - Radial Kernel"
+    gpc = "Gaussian Process Classifier"
+    mlp = "MLP Classifier"
+    ridge = "Ridge Classifier"
+    rf = "Random Forest Classifier"
+    qda = "Quadratic Discriminant Analysis"
+    ada = "Ada Boost Classifier"
+    gbc = "Gradient Boosting Classifier"
+    lda = "Linear Discriminant Analysis"
+    et = "Extra Trees Classifier"
+    xgboost = "Extreme Gradient Boosting"
+    lightgbm = "Light Gradient Boosting Machine"
+    catboost = "CatBoost Classifier"
+
+
 @app.get("/")
 async def root():
     return {pycaret.__version__}
