29c2694413e5c1c6ecfe59cd2d9a839d0489167c
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-23 07:15:33 +0200

    Update hypercorn_config.py

--- hypercorn_config.py
+++ hypercorn_config.py
@@ -1,12 +1,20 @@
 import anyio
+import os
+from dotenv import load_dotenv
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
 from main import app  # Assurez-vous que le chemin est correct selon votre structure de projet
 
-config = Config()
-config.bind = ["0.0.0.0:8000"]
+load_dotenv()
+
+def load_config() -> Config:
+    bind_addresses = os.getenv("BIND", "0.0.0.0:8000").split(",")
+    config = Config()
+    config.bind = bind_addresses
+    return config
 
 async def main():
+    config = load_config()
     async with anyio.create_task_group() as task_group:
         await task_group.spawn(serve, app, config)
 
