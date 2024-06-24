e4cf28cacc01ffb1fa61859a233bdcf05fa26e3e
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-24 08:18:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,20 +22,17 @@ class ModelType(str, Enum):
     regression = "regression"
     time_series = "time series"
 
-class ModelAnomalyDetection(str, Enum):
-    """Enumeration for different anomaly detection models."""
-    abod = "Angle-base Outlier Detection"
-    cluster = "Clustering-Based Local Outlier"
-    cof = "Connectivity-Based Outlier Factor"
-    histogram = "Histogram-based Outlier Detection"
-    iforest = "Isolation Forest"
-    knn = "k-Nearest Neighbors Detector"
-    lof = "Local Outlier Factor"
-    svm = "One-class SVM detector"
-    pca = "Principal Component Analysis"
-    mcd = "Minimum Covariance Determinant"
-    sod = "Subspace Outlier Detection"
-    sos = "Stochastic Outlier Selection"
+
+def get_pycaret_models() -> Dict[ModelType, list]:
+    models = {
+        ModelType.anomaly_detection: available_estimators(type='anomaly_detection'),
+        ModelType.classification: available_estimators(type='classification'),
+        ModelType.clustering: available_estimators(type='clustering'),
+        ModelType.regression: available_estimators(type='regression'),
+        ModelType.time_series: available_estimators(type='time_series')
+    }
+    return models
+
 
 class AnomalyDetectionSetup(BaseModel):
     """Setup parameters for anomaly detection models."""
@@ -97,6 +94,7 @@ class AnomalyDetectionSetup(BaseModel):
     profile: bool = False
     profile_kwargs: Optional[Dict[str, Any]] = None
 
+
 class AnomalyDetectionParams(BaseModel):
     """Parameters for training anomaly detection models."""
     model: str | Any
@@ -105,26 +103,6 @@ class AnomalyDetectionParams(BaseModel):
     fit_kwargs: Optional[dict] = None
     experiment_custom_tags: Optional[Dict[str, Any]] = None
 
-class ModelClassification(str, Enum):
-    """Enumeration for different classification models."""
-    lr = "Logistic Regression"
-    knn = "K Neighbors Classifier"
-    nb = "Naive Bayes"
-    dt = "Decision Tree Classifier"
-    svm = "SVM - Linear Kernel"
-    rbfsvm = "SVM - Radial Kernel"
-    gpc = "Gaussian Process Classifier"
-    mlp = "MLP Classifier"
-    ridge = "Ridge Classifier"
-    rf = "Random Forest Classifier"
-    qda = "Quadratic Discriminant Analysis"
-    ada = "Ada Boost Classifier"
-    gbc = "Gradient Boosting Classifier"
-    lda = "Linear Discriminant Analysis"
-    et = "Extra Trees Classifier"
-    xgboost = "Extreme Gradient Boosting"
-    lightgbm = "Light Gradient Boosting Machine"
-    catboost = "CatBoost Classifier"
 
 class ClassificationSetup(BaseModel):
     """Setup parameters for classification models."""
@@ -220,18 +198,6 @@ class ClassificationParams(BaseModel):
     return_train_score: bool = False
 
 
-class ModelClustering(str, Enum):
-    kmeans = "K-Means Clustering"
-    ap = "Affinity Propagation"
-    meanshift = "Mean shift Clustering"
-    sc = "Spectral Clustering"
-    hclust = "Agglomerative Clustering"
-    dbscan = "Density-Based Spatial Clustering"
-    optics = "OPTICS Clustering"
-    birch = "Birch Clustering"
-    kmodes = "K-Modes Clustering"
-
-
 class ClusteringSetup(BaseModel):
     data: Optional[dict | list | tuple | ndarray | spmatrix | DataFrame] = None
     data_func: Optional[Callable[[], dict | list | tuple | ndarray | spmatrix | DataFrame]] = None
@@ -297,34 +263,6 @@ class ClusteringParams(BaseModel):
     verbose: bool = True
     experiment_custom_tags: Optional[Dict[str, Any]] = None
     engine: Optional[str] = None
-
-
-class ModelRegression(str, Enum):
-    lr = "Linear Regression"
-    lasso = "Lasso Regression"
-    ridge = "Ridge Regression"
-    en = "Elastic Net"
-    lar = "Least Angle Regression"
-    llar = "Lasso Least Angle Regression"
-    omp = "Orthogonal Matching Pursuit"
-    br = "Bayesian Ridge"
-    ard = "Automatic Relevance Determination"
-    par = "Passive Aggressive Regressor"
-    ransac = "Random Sample Consensus"
-    tr = "TheilSen Regressor"
-    huber = "Huber Regressor"
-    kr = "Kernel Ridge"
-    svm = "Support Vector Regression"
-    knn = "K Neighbors Regressor"
-    dt = "Decision Tree Regressor"
-    rf = "Random Forest Regressor"
-    et = "Extra Trees Regressor"
-    ada = "AdaBoost Regressor"
-    gbr = "Gradient Boosting Regressor"
-    mlp = "MLP Regressor"
-    xgboost = "Extreme Gradient Boosting"
-    lightgbm = "Light Gradient Boosting Machine"
-    catboost = "CatBoost Regressor"
 
 
 class RegressionSetup(BaseModel):
@@ -419,47 +357,6 @@ class RegressionParams(BaseModel):
     return_train_score: bool = False
 
 
-class ModelTimeSeries(str, Enum):
-    naive = "Naive Forecaster"
-    grand_means = "Grand Means Forecaster"
-    snaiv = "Seasonal Naive Forecaster"
-    polytrend = "Polynomial Trend Forecaster"
-    arima = "ARIMA family of models (ARIMA, SARIMA, SARIMAX)"
-    auto_arima = "Auto ARIMA"
-    exp_smooth = "Exponential Smoothing"
-    stlf = "STL Forecaster"
-    croston = "Croston Forecaster"
-    ets = "ETS"
-    theta = "Theta Forecaster"
-    tbats = "TBATS"
-    bats = "BATS"
-    prophet = "Prophet Forecaster"
-    lr_cds_dt = "Linear w/ Cond. Deseasonalize & Detrending"
-    en_cds_dt = "Elastic Net w/ Cond. Deseasonalize & Detrending"
-    ridge_cds_dt = "Ridge w/ Cond. Deseasonalize & Detrending"
-    lasso_cds_dt = "Lasso w/ Cond. Deseasonalize & Detrending"
-    llar_cds_dt = (
-        "Lasso Least Angular Regressor w/ Cond. Deseasonalize & Detrending"
-    )
-    br_cds_dt = (
-        "Bayesian Ridge w/ Cond. Deseasonalize & Deseasonalize & Detrending"
-    )
-    huber_cds_dt = "Huber w/ Cond. Deseasonalize & Detrending"
-    omp_cds_dt = (
-        "Orthogonal Matching Pursuit w/ Cond. Deseasonalize & Detrending"
-    )
-    knn_cds_d = "K Neighbors w/ Cond. Deseasonalize & Detrending"
-    dt_cds_dt = "Decision Tree w/ Cond. Deseasonalize & Detrending"
-    rf_cds_dt = "Random Forest w/ Cond. Deseasonalize & Detrending"
-    et_cds_dt = "Extra Trees w/ Cond. Deseasonalize & Detrending"
-    gbr_cds_dt = "Gradient Boosting w/ Cond. Deseasonalize & Detrending"
-    ada_cds_dt = "AdaBoost w/ Cond. Deseasonalize & Detrending"
-    lightgbm_cds_dt = (
-        "Light Gradient Boosting w/ Cond. Deseasonalize & Detrending"
-    )
-    catboost_cds_dt = "CatBoost w/ Cond. Deseasonalize & Detrending"
-
-
 class TimeSeriesSetup(BaseModel):
     data: Optional[Series | DataFrame] = None
     data_func: Optional[Callable[[], Series | DataFrame]] = None
@@ -518,8 +415,6 @@ class TimeSeriesParams(BaseModel):
     verbose: bool = True
 
 
-
-
 def train_anomaly_detection_model(params: AnomalyDetectionParams):
     return {"model_type": "anomaly_detection", "params": params.dict()}
 
@@ -544,27 +439,18 @@ def train_time_series_model(params: TimeSeriesParams):
 async def root():
     return {"pycaret_version": pycaret.__version__}
 
-
 @app.get("/model/{model_type}")
 async def get_type(model_type: ModelType):
-    if model_type == ModelType.anomaly_detection:
-        return {model.name: model.value for model in ModelAnomalyDetection}
-    elif model_type == ModelType.classification:
-        return {model.name: model.value for model in ModelClassification}
-    elif model_type == ModelType.clustering:
-        return {model.name: model.value for model in ModelClustering}
-    elif model_type == ModelType.regression:
-        return {model.name: model.value for model in ModelRegression}
-    elif model_type == ModelType.time_series:
-        return {model.name: model.value for model in ModelTimeSeries}
+    pycaret_models = get_pycaret_models()
+    
+    if model_type in pycaret_models:
+        return pycaret_models[model_type]
     else:
         return {"error": "Model Type Unknown"}
-
 
 @app.get("/type/{model_name}")
 async def get_model(model_name: ModelType):
     return {"model_name": model_name}
-
 
 @app.post("/model/{model_type}")
 async def create_model(model_type: ModelType, params: ClassificationParams):
@@ -585,33 +471,7 @@ async def create_model(model_type: ModelType, params: ClassificationParams):
         return result
     else:
         return {"error": "Model Type Not Supported for Training"}
-
-
-@app.post("/anomaly-detection")
-async def anomaly_detection(params: AnomalyDetectionParams):
-    return train_anomaly_detection_model(params)
-
-
-@app.post("/classification")
-async def classification(params: ClassificationParams):
-    return train_classification_model(params)
-
-
-@app.post("/clustering")
-async def clustering(params: ClusteringParams):
-    return train_clustering_model(params)
-
-
-@app.post("/regression")
-async def regression(params: RegressionParams):
-    return train_regression_model(params)
-
-
-@app.post("/time-series")
-async def time_series(params: TimeSeriesParams):
-    return train_time_series_model(params)
-    
-
+  
 @app.post("/setup/anomaly_detection/")
 async def setup_anomaly_detection(setup_params: AnomalyDetectionSetup):
     """Setup anomaly detection models with the provided parameters."""
@@ -624,8 +484,11 @@ async def setup_anomaly_detection(setup_params: AnomalyDetectionSetup):
 @app.post("/train/anomaly_detection/")
 async def train_anomaly_detection(params: AnomalyDetectionParams):
     """Train anomaly detection models with the provided parameters."""
-    result = await to_thread.run_sync(pycaret.anomaly.create_model, **params.dict())
-    return result
+    try:
+        result = await to_thread.run_sync(pycaret.anomaly.create_model, **params.dict())
+        return result
+    except Exception as e:
+        return {"Error": str(e)}        
 
 @app.post("/setup/classification/")
 async def setup_classification(setup_params: ClassificationSetup):
