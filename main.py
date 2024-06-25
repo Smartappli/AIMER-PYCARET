adc92bead7cfb29690dcb9793c52ae51d433327f
Olivier DEBAUCHE <olivier.debauche@uliege.be>
2024-06-25 11:26:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 from enum import Enum
 from logging import Logger
@@ -506,9 +506,9 @@ def get_user(db, username: str):
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
