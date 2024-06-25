7a65407584b3446efb9b4ea2cb19bd64a3d069d1
Smartappli <26173534+Smartappli@users.noreply.github.com>
2024-06-25 10:08:24 +0000

    :art: Format Python code with psf/black

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,12 @@ from fastapi.testclient import TestClient
 from main import app
 from main import fake_users_db, verify_password, create_access_token
 
+
 @pytest.fixture(scope="module")
 def test_app():
     client = TestClient(app)
     yield client
+
 
 @pytest.fixture(scope="module")
 def test_user():
